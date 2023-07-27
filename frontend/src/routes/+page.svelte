72023a8a7f1273f7ccf50764990263eb623aee15
David <david.christiea@gmail.com>
2023-07-26 22:16:04 -0400

    Initial commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type {PageData} from "./$types";
+    import {invalidateAll} from "$app/navigation";
+
+    export let data: PageData;
+    let todos = data.todos;
+
+    async function deleteTodo(id: number) {
+        await fetch(`http://localhost:8000/delete/${id}`);
+        await invalidateAll();
+    }
+
+    async function updateTodo(todo) {
+        await fetch(`http://localhost:8000/update?id=${todo.id}&description=${todo.description}&done=${todo.done}`);
+    }
+</script>
+
+<div class="container mx-auto mt-16">
+    <h1 class="h1 text-center">Todos</h1>
+    <form action="http://localhost:8000/create" method="POST">
+        <input type="text" class="input p-4 my-8" name="description" placeholder="What needs to be done?" autocomplete="off" />
+    </form>
+
+    <div class="space-y-4">
+        {#each data.todos as todo }
+            <div class="flex items-center justify-between p-4 bg-surface-800 rounded-lg gap-4">
+                <input type="checkbox" class="checkbox" bind:checked={todo.done} on:change={() => updateTodo(todo)} />
+                <input type="text" class="input" bind:value={todo.description} placeholder="Description" />
+                <div class="flex gap-2">
+                    <button class="btn variant-filled-primary" on:click={() => {updateTodo(todo)}}>Update</button>
+                    <button class="btn variant-filled-primary" on:click={() => {deleteTodo(todo.id)}}>Delete</button>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
